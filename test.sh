9c6c404d19be143a93f3dd290a3c9e8c0a7e582d
Benjamin Boortz <benjamin.boortz@secure.mailbox.org>
2019-04-20 18:11:11 +0200

    add: dev: test.sh added

---
+++ test.sh
@@ -0,0 +1,152 @@
+#!/bin/sh
+
+
+#
+# confg
+#
+HOST=0.0.0.0
+PORT=5000
+CURL="curl --fail --silent -o /dev/null -w %{http_code}"
+API_KEY="KDDhzT5FvHzxqs2f-77QNysPgh0iughuj-xULyaYEkav9oeLT7"
+ERRORS=0
+
+
+
+#
+# method
+#
+do_curl() {
+	local option="$1"
+	local url="$2"
+	local content_type="${3}"
+	local expected_code="${4}"
+	shift
+	shift
+	shift
+	shift
+	local data="$@"
+
+	curl_cmd="$CURL -X ${option} ${url}"
+
+	local http_code
+	if [ -z "${data}" ]; then
+		echo -e "\nCURL: ${curl_cmd}" -H "Content-Type: ${content_type}"
+		http_code=$( ${curl_cmd} -H "Content-Type: ${content_type}" )
+	else
+		echo -e "\nCURL: ${curl_cmd}" -H "Content-Type: ${content_type} -d ${data}"
+		http_code=$( ${curl_cmd} -H "Content-Type: ${content_type}" -d "${data}" -H "X-API_KEY: ${API_KEY}" )
+	fi
+	echo "  http_code: ${http_code}"
+	if [ "${http_code}" != "${expected_code}" ]; then
+		let ERRORS=$ERRORS+1
+		echo "  ERROR!"
+		echo "    http_code: ${http_code} != ${expected_code}"
+	else
+		echo "  SUCCESS!"
+	fi
+}
+
+
+
+#
+# prepare
+# 
+source .venv/bin/activate
+
+
+
+#
+# pytest
+#
+echo -e "\nRUNNING PYTEST UNIT TESTS"
+cd app
+pytest
+cd ..
+
+
+
+#
+# running application in dev mode
+#
+echo -e "\nRUNNING THE APPLICATION"
+DEV=1 ./run.sh > testrun.log 2>&1 &
+
+
+
+#
+# 
+#
+echo -e "\nSIMULATING TESTS"
+sleep 1
+do_curl "GET"    "http://localhost:5000"              "text/html"        "200"
+do_curl "GET"    "http://localhost:5000/"             "application/json" "200"
+do_curl "GET"    "http://localhost:5000/api/motd"     "application/json" "200"
+do_curl "POST"   "http://localhost:5000/api/motd"     "application/json" "403"
+do_curl "DELETE" "http://localhost:5000/api/motd"     "application/json" "200" '1'
+
+sleep 1
+do_curl "POST"   "http://localhost:5000/api/services" "application/json" "405"
+do_curl "POST"   "http://localhost:5000/api/service"  "application/json" "403"
+do_curl "POST"   "http://localhost:5000/api/service"  "application/json" "201" '{ "name": "service0", "score": 100 }'
+do_curl "POST"   "http://localhost:5000/api/service"  "application/json" "201" '{ "name": "service1", "score": 100 }'
+do_curl "POST"   "http://localhost:5000/api/motd"     "application/json" "201" '{ "message": "services are ok" }'
+do_curl "GET"    "http://localhost:5000/api/services" "application/json" "200"
+do_curl "GET"    "http://localhost:5000/api/events" "application/json" "200"
+
+sleep 1
+do_curl "DELETE" "http://localhost:5000/api/motd"     "application/json" "200" '1'
+do_curl "POST"   "http://localhost:5000/api/service"  "application/json" "201" '{ "name": "service0", "score": 100 }'
+do_curl "POST"   "http://localhost:5000/api/service"  "application/json" "201" '{ "name": "service1", "score": 100 }'
+do_curl "POST"   "http://localhost:5000/api/motd"     "application/json" "201" '{ "message": "services are still ok" }'
+do_curl "POST"   "http://localhost:5000/api/event"    "application/json" "201" '{ "title": "change scheduled", "description": "A change has been scheduled." }'
+do_curl "GET"    "http://localhost:5000/api/motd"     "application/json" "200"
+do_curl "GET"    "http://localhost:5000/api/services" "application/json" "200"
+do_curl "GET"    "http://localhost:5000/api/events" "application/json" "200"
+
+sleep 1
+do_curl "POST"   "http://localhost:5000/api/service"  "application/json" "201" '{ "name": "service0", "score": 100 }'
+do_curl "POST"   "http://localhost:5000/api/service"  "application/json" "201" '{ "name": "service1", "score": 100 }'
+do_curl "POST"   "http://localhost:5000/api/service"  "application/json" "201" '{ "name": "service2", "score": 100 }'
+do_curl "POST"   "http://localhost:5000/api/motd"     "application/json" "201" '{ "message": "services are still ok" }'
+do_curl "GET"    "http://localhost:5000/api/motd"     "application/json" "200"
+do_curl "GET"    "http://localhost:5000/api/services" "application/json" "200"
+do_curl "GET"    "http://localhost:5000/api/events" "application/json" "200"
+
+sleep 1
+do_curl "POST"   "http://localhost:5000/api/service"  "application/json" "201" '{ "name": "service0", "score": 100 }'
+do_curl "POST"   "http://localhost:5000/api/service"  "application/json" "201" '{ "name": "service1", "score": 100 }'
+do_curl "POST"   "http://localhost:5000/api/service"  "application/json" "201" '{ "name": "service2", "score": 50 }'
+do_curl "POST"   "http://localhost:5000/api/motd"     "application/json" "201" '{ "message": "services2 is broken" }'
+do_curl "GET"    "http://localhost:5000/api/motd"     "application/json" "200"
+do_curl "GET"    "http://localhost:5000/api/services" "application/json" "200"
+do_curl "GET"    "http://localhost:5000/api/events" "application/json" "200"
+
+sleep 1
+do_curl "POST"   "http://localhost:5000/api/service"  "application/json" "201" '{ "name": "service0", "score": 100 }'
+do_curl "POST"   "http://localhost:5000/api/service"  "application/json" "201" '{ "name": "service1", "score": 100 }'
+do_curl "POST"   "http://localhost:5000/api/service"  "application/json" "201" '{ "name": "service2", "score": 50 }'
+do_curl "POST"   "http://localhost:5000/api/motd"     "application/json" "201" '{ "message": "services2 is broken" }'
+do_curl "GET"    "http://localhost:5000/api/motd"     "application/json" "200"
+do_curl "GET"    "http://localhost:5000/api/services" "application/json" "200"
+do_curl "GET"    "http://localhost:5000/api/events" "application/json" "200"
+
+sleep 1
+do_curl "POST"   "http://localhost:5000/api/service"  "application/json" "201" '{ "name": "service0", "score": 100 }'
+do_curl "POST"   "http://localhost:5000/api/service"  "application/json" "201" '{ "name": "service1", "score": 100 }'
+do_curl "POST"   "http://localhost:5000/api/service"  "application/json" "201" '{ "name": "service2", "score": 100 }'
+do_curl "POST"   "http://localhost:5000/api/event"    "application/json" "201" '{ "title": "service2 had an outage", "description": "After the change service2 was broken. Now it is repaired again." }'
+do_curl "POST"   "http://localhost:5000/api/motd"     "application/json" "201" '{ "message": "services are ok again" }'
+do_curl "GET"    "http://localhost:5000/api/motd"     "application/json" "200"
+do_curl "GET"    "http://localhost:5000/api/services" "application/json" "200"
+do_curl "GET"    "http://localhost:5000/api/events" "application/json" "200"
+
+
+
+
+#
+# finalize
+#
+kill %1
+deactivate
+echo -e "\n\n$ERRORS ERRORS FOUND!"
+exit $ERRORS
